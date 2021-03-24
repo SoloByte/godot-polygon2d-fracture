c200508ed21d5ecffc10043291d06818faccec0c
Dave G <34277803+SoloByte@users.noreply.github.com>
2021-03-24 10:58:09 +0100

    project commit.

---
+++ FractureBody.gd
@@ -0,0 +1,26 @@
+extends RigidBody2D
+
+
+
+
+onready var _col_poly := $CollisionPolygon2D
+onready var _poly := $Polygon2D
+onready var _line := $Line2D
+
+
+
+
+
+
+
+
+
+func setPolygon(polygon) -> void:
+	_col_poly.set_polygon(polygon)
+	_poly.set_polygon(polygon)
+	polygon.append(polygon[0])
+	_line.points = polygon
+
+
+func setColor(color : Color) -> void:
+	modulate = color
---
+++ final/FracturePolygonLib.gd
@@ -0,0 +1,10 @@
+class_name FracturePolygonLib
+
+
+
+
+
+
+
+
+
