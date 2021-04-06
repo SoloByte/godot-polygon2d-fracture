10ea691792a0a48ec0ca3bff71756934d6bb1ea0
Dave G <34277803+SoloByte@users.noreply.github.com>
2021-04-06 15:26:33 +0200

    cutFracture.gd clean up. Cut line logic improvements.

--- demo/src/CutFracture.gd
+++ demo/src/CutFracture.gd
@@ -3,16 +3,20 @@ extends Node2D
 
 
 
-const MIN_CUT_SPEED : float = 1.0
-const MIN_CUT_LENGTH : float = 50.0
-const MAX_CUT_LENGTH : float = 2500.0
-const CUT_LINE_SEGMENT_MIN_LENGTH : float = 100.0
+const CUT_LINE_POINT_MIN_DISTANCE : float = 25.0
+const CUT_LINE_STATIONARY_DELAY : float = 0.1
+const CUT_LINE_DIRECTION_THRESHOLD : float = -0.7 #smaller than treshold ends line (start_dir.dot(cur_dir) < threshold = endLine)
+
+const CUT_LINE_MIN_LENGTH : float = 100.0
+const CUT_LINE_SEGMENT_MIN_LENGTH : float = 50.0
+
 
 
 
 export(Color) var fracture_body_color
 export(PackedScene) var fracture_body_template
 export(PackedScene) var rigidbody_template
+
 
 
 
@@ -25,23 +29,21 @@ onready var _slowmo_timer := $SlowMoTimer
 onready var _cut_line := $CutLine
 
 
-var _cur_fracture_color : Color = fracture_body_color
-#var _cut_line_start := Vector2.ZERO
 
-
-var _cut_line_total_length : float = 0.0
-
-var _cut_line_points : PoolVector2Array = []
-
-var _cut_line_enabled : bool = false
-
-var _last_mouse_pos := Vector2.ZERO
 
 var _input_disabled : bool = false
 
+var _cur_fracture_color : Color = fracture_body_color
+
+var _cut_line_enabled : bool = false
+var _cut_line_total_length : float = 0.0
+var _cut_line_points : PoolVector2Array = []
+var _cut_line_start_direction := Vector2.ZERO
+var _cut_line_t : float = 0.0
 
 
-#var _first_cut_direction := Vector2.ZERO
+
+
 
 
 
@@ -57,39 +59,14 @@ func _ready() -> void:
 	
 	_cut_line.clear_points()
 	_cut_line.visible = false
-	
-	for source in _source_polygon_parent.get_children():
-		if source is Polygon2D:
-			source.global_rotation = _rng.randf() * PI * 2.0
-
 
 
 func _process(delta: float) -> void:
 	if _cut_line_enabled:
 		var cur_pos : Vector2 = get_global_mouse_position()
-		calculateCutLine(cur_pos)
-		_last_mouse_pos = cur_pos
-
-
-
-func spawnPoly(new_poly : PoolVector2Array, spawn_pos : Vector2, spawn_rot : float, color : Color) -> void:
-	var p = Polygon2D.new()
-	_source_polygon_parent.add_child(p)
-	p.global_position = spawn_pos
-	p.set_polygon(new_poly)
-	p.modulate = color
-
-func spawnRigibody2d(new_poly : PoolVector2Array, spawn_pos : Vector2, spawn_rot : float, color : Color, lin_vel : Vector2, ang_vel : float, mass : float, cut_pos : Vector2) -> void:
-	var instance = rigidbody_template.instance()
-	_source_polygon_parent.add_child(instance)
-	instance.global_position = spawn_pos
-	instance.set_polygon(new_poly)
-	instance.modulate = color
-	instance.linear_velocity = lin_vel + (spawn_pos - cut_pos).normalized() * 50
-	instance.angular_velocity = ang_vel
-	instance.mass = mass
-
-
+		if _cut_line_t < 1.0:
+			_cut_line_t += delta * (1.0 / CUT_LINE_STATIONARY_DELAY)
+		calculateCutLine(cur_pos, _cut_line_t)
 
 
 func _input(event: InputEvent) -> void:
@@ -99,54 +76,57 @@ func _input(event: InputEvent) -> void:
 		if event.button_index == 1:
 			if _cut_line_enabled:
 				if not event.pressed:
-					endCut()
+					endCutLine()
 					_cut_line_enabled = false
+					_cut_line.visible = false
 			else:
 				if event.pressed:
 					_cut_line_enabled = true
+					_cut_line.visible = true
 		elif event.button_index == 2 and not _cut_line_enabled:
 			if event.pressed:
 				simpleCut(get_global_mouse_position())
 
 
-func calculateCutLine(cur_pos : Vector2) -> void:
+
+
+
+func calculateCutLine(cur_pos : Vector2, t : float) -> void:
 	if _cut_line_points.size() <= 0:
 		_cut_line_points.append(cur_pos)
 	else:
-		var last_pos : Vector2 = _last_mouse_pos
+		var last_pos : Vector2 = lerp(_cut_line_points[_cut_line_points.size() - 1], cur_pos, t)
 		var vec : Vector2 = cur_pos - last_pos
-		var dis : float = vec.length()
 		var dir : Vector2 = vec.normalized()
 		
-		_cut_line_total_length += dis
-		if not _cut_line.visible and _cut_line_total_length > MIN_CUT_LENGTH:
-			_cut_line.visible = true
-		
-		if _cut_line_total_length > MAX_CUT_LENGTH:
-			endCut()
+		if _cut_line_start_direction == Vector2.ZERO:
+			_cut_line_start_direction = dir
+		elif dir == Vector2.ZERO:
+			endCutLine()
 			return
+		else:
+			if _cut_line_start_direction.dot(dir) < CUT_LINE_DIRECTION_THRESHOLD:
+				endCutLine()
+				return
 		
-#		if _first_cut_direction == Vector2.ZERO:
-#			_first_cut_direction = dir
-#		else:
-#			if _first_cut_direction.dot(dir) < -0.5:
-#				endCut()
-#				return
-			
-		if dis > MIN_CUT_SPEED:
+		var last_point : Vector2 = _cut_line_points[_cut_line_points.size() - 1]
+		var dis : float = (cur_pos - last_point).length()
+		if dis > CUT_LINE_POINT_MIN_DISTANCE:
 			_cut_line_points.append(cur_pos)
 			_cut_line.points = _cut_line_points
+			_cut_line_t = 0.0
+			_cut_line_total_length += dis
 		else:
-			endCut()
-			return
+			_cut_line.points = _cut_line_points
+			_cut_line.add_point(cur_pos, -1)
 
 
-func startCut() -> void:
+func startCutLine() -> void:
 	pass
 
 
-func endCut() -> void:
-	if _cut_line_points.size() > 1:
+func endCutLine() -> void:
+	if _cut_line_points.size() > 1 and _cut_line_total_length > CUT_LINE_MIN_LENGTH:
 		var final_shape : PoolVector2Array = []
 		var i : int = 0
 		var construct_point := Vector2.ZERO
@@ -168,15 +148,16 @@ func endCut() -> void:
 					i = j
 					break
 		
-		cut(_cut_line_points[0], final_shape, 0.0)
+		cutSourcePolygons(_cut_line_points[0], final_shape, 0.0)
 	
 	
 	
-	_cut_line.visible = false
+#	_cut_line.visible = false
 	_cut_line_total_length = 0.0
 	_cut_line_points = []
 	_cut_line.clear_points()
-#	_first_cut_direction = Vector2.ZERO
+	_cut_line_start_direction = Vector2.ZERO
+	_cut_line_t = 0.0
 	
 	_input_disabled = true
 	set_deferred("_input_disabled", false)
@@ -185,42 +166,12 @@ func endCut() -> void:
 
 func simpleCut(pos : Vector2) -> void:
 	var cut_pos : Vector2 = pos
-	cut(cut_pos, _cut_shape, 0.0)
+	cutSourcePolygons(cut_pos, _cut_shape, 0.0)
 	_input_disabled = true
 	set_deferred("_input_disabled", false)
 
-#func _input(event: InputEvent) -> void:
-#	if _input_disabled: return
-#	if event.is_action_pressed("cut_line"):
-#		_cut_line_start = get_global_mouse_position()
-#		var points : PoolVector2Array = [_cut_line_start, _cut_line_start]
-#		_cut_line.visible = true
-#		_cut_line.points = points
-#
-#
-#	if event.is_action_released("cut_line"):
-#		var cut_line_end : Vector2 = get_global_mouse_position()
-#		var vec : Vector2 = cut_line_end - _cut_line_start
-#		var dis : float = vec.length()
-#		var dir : Vector2 = vec.normalized()
-#		var cut_shape = PolygonLib.createBeamPolygon(dir, dis, 1.0, 1.0, Vector2.ZERO)
-#		cut(_cut_line_start, cut_shape, 0.0)
-#
-#		_cut_line.clear_points()
-#		_cut_line.visible = false
-#		_input_disabled = true
-#		set_deferred("_input_disabled", false)
-#
-#
-#
-#	if event.is_action_pressed("cut"):
-#		var cut_pos : Vector2 = get_global_mouse_position()
-#		cut(cut_pos, _cut_shape, 0.0)
-#		_input_disabled = true
-#		set_deferred("_input_disabled", false)
 
-
-func cut(cut_pos : Vector2, cut_shape : PoolVector2Array, cut_rot : float) -> void:
+func cutSourcePolygons(cut_pos : Vector2, cut_shape : PoolVector2Array, cut_rot : float) -> void:
 	for source in _source_polygon_parent.get_children():
 		var source_polygon : PoolVector2Array = source.get_polygon()
 		var total_area : float = PolygonLib.getPolygonArea(source_polygon)
@@ -258,14 +209,28 @@ func cut(cut_pos : Vector2, cut_shape : PoolVector2Array, cut_rot : float) -> vo
 				call_deferred("spawnRigibody2d", shape.centered_shape, spawn_pos, 0.0, source.modulate, s_lin_vel, s_ang_vel, mass, cut_pos)
 		
 		source.queue_free()
-	
-#	Engine.time_scale = 0.1
-#	_slowmo_timer.start(0.25)
 
 
 
 
 
+func spawnPoly(new_poly : PoolVector2Array, spawn_pos : Vector2, spawn_rot : float, color : Color) -> void:
+	var p = Polygon2D.new()
+	_source_polygon_parent.add_child(p)
+	p.global_position = spawn_pos
+	p.set_polygon(new_poly)
+	p.modulate = color
+
+
+func spawnRigibody2d(new_poly : PoolVector2Array, spawn_pos : Vector2, spawn_rot : float, color : Color, lin_vel : Vector2, ang_vel : float, mass : float, cut_pos : Vector2) -> void:
+	var instance = rigidbody_template.instance()
+	_source_polygon_parent.add_child(instance)
+	instance.global_position = spawn_pos
+	instance.set_polygon(new_poly)
+	instance.modulate = color
+	instance.linear_velocity = lin_vel# + (spawn_pos - cut_pos).normalized() * 50
+	instance.angular_velocity = ang_vel
+	instance.mass = mass
 
 
 func spawnFractureBody(fracture_shard : Dictionary) -> void:
@@ -275,6 +240,8 @@ func spawnFractureBody(fracture_shard : Dictionary) -> void:
 	var dir : Vector2 = (to_global(fracture_shard.centroid) - global_position).normalized()
 	instance.apply_central_impulse(dir * _rng.randf_range(300, 500))
 	instance.angular_velocity = _rng.randf_range(-1, 1)
+
+
 
 
 func _on_SlowMoTimer_timeout() -> void:
