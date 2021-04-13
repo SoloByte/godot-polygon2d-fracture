7062c4be2c242f77731e1b688b166b7ac71c4b20
Dave G <34277803+SoloByte@users.noreply.github.com>
2021-04-13 10:12:32 +0200

    license update.

--- demo/src/CutFracture.gd
+++ demo/src/CutFracture.gd
@@ -9,7 +9,7 @@ extends Node2D
 #                     GODOT Polygon 2D Fracture                         
 #           https://github.com/SoloByte/godot-polygon2d-fracture          
 # -----------------------------------------------------------------------
-# Copyright (c) 2021 Dave Grueneis
+# Copyright (c) 2021 David Grueneis
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
